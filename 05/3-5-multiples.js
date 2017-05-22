956cc75635050074f8dad052ed8be05a744b3e69
BobbyTables <robert@creativequeries.com>
2017-05-22 19:57:42 -0400

    Add functions for solving problem.

--- 05/3-5-multiples.js
+++ 05/3-5-multiples.js
@@ -12,11 +12,17 @@ console.log("The sum is: ", sum);
 var sum2 = 0;
 var n = 0;
 while(n < 1000) {
-  if(n % 5 == 0 || n % 3 == 0) {
-    sum2 += n
-  }
-
+  sum2 += validSum(n);
   n++;
 }
 
-console.log("The sum2 is: ", sum2);
+
+function validSum(number) {
+  if(number % 5 == 0 || number % 3 == 0) {
+    return number;
+  } else {
+    return 0;
+  }
+}
+
+console.log("The sum2 is:", sum2);
