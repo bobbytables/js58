57c6667a41cd3d55ffbc04e6660ad257b5a129f1
BobbyTables <robert@creativequeries.com>
2017-06-05 20:42:19 -0400

    Add box container logic.

--- 08/exercise01/app.js
+++ 08/exercise01/app.js
@@ -15,4 +15,20 @@ $(function(){
 
     $(this).text("You clicked me " + counter + " times - " + text);
   });
+
+  // Click on a box logic
+  // $('div.box-container button').click(function(event){
+  //   $(this).hide();
+  // })
+
+  $('div.box-container button').on('click', function(event){
+    // retrieve the value from the number input field
+    var chicken = $('#number-input').val();
+
+    // change the text of the button to the value of the number input
+    $(this).text(chicken);
+
+    // Set the input value to a blank string
+    $('#number-input').val("");
+  });
 })
